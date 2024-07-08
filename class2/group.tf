657728b47131c6c6dd71115d73310b9a167f87e2
Aleksandr-Dymbrylov <alexdymbrylov@gmail.com>
2024-07-08 20:56:28 +0000

    added class2

---
+++ class2/group.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_group" "developers2" {
+  name = "developers2"
+}
+
+resource "aws_iam_group" "developers3" {
+  name = "developers3"
+}
