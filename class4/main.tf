9c135e09542d23ced0293b9ccf99fee77913fb96
Aleksandr-Dymbrylov <alexdymbrylov@gmail.com>
2024-07-13 20:07:13 +0000

    add class5

--- class4/main.tf
+++ class4/main.tf
@@ -5,23 +5,27 @@ provider aws {
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  availability_zone = var.kaizen
 }
 
+variable kaizen {}
+
 variable ami_id {
-    description = "provide ami id"
+  description = "Provide ami id"
+  type = string
+  default = ""
+}
+
+variable instance_type {
+    description = "Provide instance type"
     type = string
     default = ""
 }
 
-variable instance_type {
-  description = "provide instance type"
-  type = string
-  default = ""
+variable region {
+    description = "Provide region"
+    type = string
+    default = ""
 }
 
-variable region {
-  description = "provide region"
-  type = string
-  default = ""
-}
 # ami-08be1e3e6c338b037
